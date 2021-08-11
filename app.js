1825cf57ed2966d6266e7df3257d0e96338a296f
toastDerivation <jmgnngmj@gmail.com>
2021-08-11 14:24:46 +0800

    81121

--- app.js
+++ app.js
@@ -11,16 +11,98 @@ app.get('/' , (req, res) =>{
 
 app.get('/search' , (req, res) =>{
 
-    let temp_student_lists = student_lists.filter(elem =>{
-        if(req.query.last_name){
-            return elem.last_name.toUpperCase() == req.query.last_name.toUpperCase()
-        } 
-    })
-    
-    if(temp_student_lists.length < 1){
-        temp_student_lists = student_lists
-    }
+    let temp_student_lists = student_lists
 
+    if(req.query.last_name){
+        let temp_last_name, temp_first_name = "", temp_middle_name , temp_user_id
+        let temp_last_name_arr = []
+        let start_index_first_name , end_index_first_name
+        let prev_key, prev_prev_key
+        let search_index = 0, search_index_length = 0
+        let two_surnames = ["DE", "DEL" , "DELA" , "DELOS", "SAN"]
+        let temp_arranged_student_lists = []
+
+        for(let i = 0; i < student_lists.length; i++){ 
+            if(two_surnames.includes(student_lists[i].last_name)) {
+                temp_last_name_arr.push(student_lists[i].last_name + " " + student_lists[i].first_name)
+            }else{ 
+                temp_last_name_arr.push(student_lists[i].last_name)
+            } 
+        }
+        
+        for(let i = 0; i < student_lists.length; i++){
+            if(req.query.last_name.toUpperCase() == temp_last_name_arr[i]){
+                temp_last_name = temp_last_name_arr[i]
+                search_index = i
+
+                if(two_surnames.includes(student_lists[i].last_name)) {
+                    start_index_first_name = 2
+                }else{ 
+                    start_index_first_name = 1
+                } 
+
+                break
+            }
+        }
+
+        for (let [key, value] of Object.entries(student_lists[search_index])){
+            if(key == 'id'){
+                break
+            }
+            prev_key = key
+        }
+
+        for (let [key, value] of Object.entries(student_lists[search_index])){
+            if(key == prev_key){
+                break
+            }
+            prev_prev_key = key
+        }
+
+        for (let [key, value] of Object.entries(student_lists[search_index])){
+            search_index_length += 1
+        }
+
+        if(two_surnames.includes(student_lists[search_index][prev_prev_key])){
+            end_index_first_name = search_index_length - 3
+            temp_middle_name = student_lists[search_index][prev_prev_key] + " " + student_lists[search_index][prev_key]
+        }else{
+            end_index_first_name = search_index_length - 2
+            temp_middle_name = student_lists[search_index][prev_key]
+        }
+
+        let b = 0
+        let add_first_name = false
+
+        for (let [key, value] of Object.entries(student_lists[search_index])){
+            if(b == start_index_first_name){
+                add_first_name = true
+            }
+
+            if(b == end_index_first_name){
+                break
+            }
+
+            if(add_first_name){
+                temp_first_name = temp_first_name + value + " "
+            }
+            b += 1
+        }
+
+        for(let i = 0; i < student_lists.length; i++){
+
+        }
+
+        temp_student_lists = student_lists.filter(elem =>{
+            return (
+                temp_first_name == req.query.first_name.toUpperCase() &&
+                temp_middle_name == req.query.middle_name.toUpperCase() &&
+                temp_last_name == req.query.last_name.toUpperCase() 
+                //elem.id == req.query.user_id
+            )
+        })
+    }
+ 
     res.render('search', {
         search_data: req.query,
         search_information : {
